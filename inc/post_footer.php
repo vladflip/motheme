daf3c3d9ed34e52351ce0179afa45545c88f181a
flip <my@email.com>
2016-11-22 22:30:33 +0200

    post views

--- inc/post_footer.php
+++ inc/post_footer.php
@@ -2,13 +2,21 @@
 	<hr class="post_separator">
 <?php endif ?>
 <div class="post_footer">
-	<div class="post_date">
-		<?= getTheDate($post->ID); ?>
+
+	<div class="post_data">
+		<div class="post_views" title="Просмотры"><?= pvc_get_post_views() ?></div>
+		<div class="post_date">
+			<?= getTheDate($post->ID); ?>
+		</div>
 	</div>
-	<div class="post_share">
-		<script type="text/javascript">
-		<?php $link = get_permalink();?>
-		document.write(VK.Share.button({url: "<?=$link?>"},{type: "custom", text: "<img src=\"http://vk.com/images/share_32_eng.png\" width=\"32\" height=\"32\" title=\"Поделиться\" />", eng: 1}));
-		</script>
+
+	<div class="post_socials">
+		<div class="post_share">
+			<script type="text/javascript">
+			<?php $link = get_permalink();?>
+			document.write(VK.Share.button({url: "<?=$link?>"},{type: "custom", text: "<img src=\"http://vk.com/images/share_32_eng.png\" width=\"32\" height=\"32\" title=\"Поделиться\" />", eng: 1}));
+			</script>
+		</div>
 	</div>
+
 </div>
