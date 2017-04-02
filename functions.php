6ffef103f75e09321dc4bce8848f59937a956ae2
flip <my@email.com>
2017-04-02 10:17:10 -0600

    allow editors edit menu

--- functions.php
+++ functions.php
@@ -12,6 +12,25 @@ add_action('init', 'include_scripts_and_styles');
 add_action('init', 'register_custom_posts');
 add_action('init', 'registerMenu');
 
+// Allow editors to see Appearance menu
+$role_object = get_role( 'editor' );
+$role_object->add_cap( 'edit_theme_options' );
+function hide_menu() {
+ 
+    // Hide theme selection page
+    remove_submenu_page( 'themes.php', 'themes.php' );
+ 
+    // Hide widgets page
+    remove_submenu_page( 'themes.php', 'widgets.php' );
+ 
+    // Hide customize page
+    global $submenu;
+    unset($submenu['themes.php'][6]);
+ 
+}
+ 
+add_action('admin_head', 'hide_menu');
+
 // ---------------------------------------------
 // CREATE CUSTOM POSTS
 // ---------------------------------------------
